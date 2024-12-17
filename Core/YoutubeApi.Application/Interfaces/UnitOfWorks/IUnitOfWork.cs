8232cc486c42c2893ad7e4449d2151cbe22e6b77
Ramazan-Alkaptan <alkaptan5255@gmail.com>
2024-12-17 16:57:24 +0300

    Registration güncellendi

---
+++ Core/YoutubeApi.Application/Interfaces/UnitOfWorks/IUnitOfWork.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using YoutubeApi.Application.Interfaces.Repositories;
+using YoutubeApi.Domain.Common;
+
+namespace YoutubeApi.Application.Interfaces.UnıtOfWorks
+{
+    public interface IUnitOfWork : IAsyncDisposable
+    {
+        IReadRepository<T> GetReadRepository<T>() where T : class, IEntityBase, new(); 
+        IWriteRepository<T> GetWriteRepository<T>() where T : class, IEntityBase, new();
+        Task<int> SaveAsync();
+        int Save();
+    }
+}
